86c9ddbaf00ccb2c53a9fe50c6f63111e537357f
Harshasri-uppara <85478788+Harshasri-uppara@users.noreply.github.com>
2021-08-10 18:01:01 +0530

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE customer
 (
-    customer_id	SERIAL PRIMARY KEY,
-    customer_name	varchar(30),
-    customer_email	varchar(80)
+    customer_id SERIAL PRIMARY KEY,
+    customer_name varchar(30),
+    customer_email varchar(80)
 );
 
 CREATE TABLE product
@@ -15,25 +15,25 @@ CREATE TABLE product
 
 CREATE TABLE orders
 (
-    order_id	SERIAL PRIMARY KEY,
-    order_date	date,
+    order_id SERIAL PRIMARY KEY,
+    order_date date,
     order_status varchar(20),
-    customer_id	int REFERENCES customer (customer_id)
+    customer_id int REFERENCES customer (customer_id)
 );
 
 CREATE TABLE order_details
 (
-    order_details_id	SERIAL PRIMARY KEY,
-    order_id		int REFERENCES orders (order_id),
-    product_id		int REFERENCES product (product_id),
-    quantity		int,
-    total_price		int
+    order_details_id SERIAL PRIMARY KEY,
+    order_id int REFERENCES orders (order_id),
+    product_id int REFERENCES product (product_id),
+    quantity int,
+    total_price int
 );
 
 CREATE TABLE payment
 (
-    payment_id		SERIAL PRIMARY KEY,
-    payment_date        date,
-    amount_paid		int,
-    order_id		int REFERENCES orders (order_id)
+    payment_id SERIAL PRIMARY KEY,
+    payment_date date,
+    amount_paid int,
+    order_id int REFERENCES orders (order_id)
 );
